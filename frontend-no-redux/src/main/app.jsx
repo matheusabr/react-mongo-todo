5a5bcf527f6be54a70091e628ac9e031fbe4311f
Matheus Abreu <mabreu@PDM-00870.lan>
2019-02-24 22:02:47 +0000

    feat: navigate to Todo and About pages with routes

--- frontend-no-redux/src/main/app.jsx
+++ frontend-no-redux/src/main/app.jsx
@@ -6,9 +6,12 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import '../template/custom.css'
 
 import React from 'react'
+import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
   <div className='container'>
-    <h1>To Do App</h1>
+    <Menu />
+    <Routes />
   </div>
 )
