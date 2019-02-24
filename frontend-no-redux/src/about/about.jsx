4a279ef60fe5ebc049958a08a74f450772d7a553
Matheus Abreu <mabreu@PDM-00870.lan>
2019-02-24 21:59:37 +0000

    chore(about): create about page

---
+++ frontend-no-redux/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='About' small='App' />
+    <p>Lorem ipsum dolor sit amet...</p>
+
+    <h4>Version</h4>
+    <p>1.0.0</p>
+  </div>
+)
