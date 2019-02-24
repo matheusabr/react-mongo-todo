0e101504ce361c99b4e43c29917e401adaa53552
Matheus Abreu <mabreu@PDM-00870.lan>
2019-02-24 21:31:52 +0000

    chore(frontend): create app and default components

---
+++ frontend-no-redux/src/main/app.jsx
@@ -0,0 +1,14 @@
+// Bootstrap
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+// Font Awesome
+import 'modules/font-awesome/css/font-awesome.min.css'
+// Custom CSS
+import '../template/custom.css'
+
+import React from 'react'
+
+export default props => (
+  <div className='container'>
+    <h1>To Do App</h1>
+  </div>
+)
